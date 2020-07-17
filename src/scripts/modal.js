7aa15af1e1dba89b208dd5c9d35f708ccc8c458d
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-07-17 14:45:38 -0300

    modal.js

---
+++ src/scripts/modal.js
@@ -0,0 +1,12 @@
+  
+let buttonSearch = document.querySelector('#page-home main a')
+let modal = document.querySelector('.modal')
+let close = document.querySelector('.modal .header a')
+
+buttonSearch.addEventListener('click', function(){
+    modal.classList.remove('hide')
+})
+
+close.addEventListener('click', function(){
+    modal.classList.add('hide')
+})
