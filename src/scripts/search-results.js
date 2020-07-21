42193e25fa1db64218ec2934b551910e585cb9ee
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-07-21 20:05:17 -0300

    refatorando

--- src/scripts/search-results.js
+++ src/scripts/search-results.js
@@ -1,15 +1,16 @@
 const api = 'http://localhost:8000/posts'
-var res = document.querySelector('#res')
 var card = document.querySelector('#card')
 
 
 var images = [
-    image1 = '../src//assets/pawel-czerwinski-RkIsyD_AVvc-unsplash.jpg',
-    image2 = '../src//assets/michael-jin-ET6_fDwZj2U-unsplash.jpg',
-    image3 = '../src//assets/javier-huedo-io3poUllunQ-unsplash.jpg'
+    image1 = './src//assets/pawel-czerwinski-RkIsyD_AVvc-unsplash.jpg',
+    image2 = './src//assets/michael-jin-ET6_fDwZj2U-unsplash.jpg',
+    image3 = './src//assets/javier-huedo-io3poUllunQ-unsplash.jpg'
 ]
 
 console.log(images.length)
+
+images.forEach()
 var random = Math.floor(Math.random() * images.length);
 
 const options = {
@@ -30,13 +31,14 @@ fetch(api, options)
         arrayData.forEach(function (element) {
             console.log(element)
 
+            
             card.innerHTML +=
                 "<img src=" + images[random] + " alt='imagem'>'" + "<br>"
                 + "<h1>" + element.ponto + " </h1>" + "<br>"
                 + '<h3>' + element.items + '</h3>' + "<br>"
-
                 + '<p>' + element.state + ', ' + element.city + ', ' + ' <br> '
                 + element.address + 'nº ' + element.address2 
+            
         })
 
         /*
@@ -59,7 +61,7 @@ fetch(api, options)
     .catch(error => console.error('Error:', error))
 
 
-
+/*
 var images = [
     a = '../src//assets/pawel-czerwinski-RkIsyD_AVvc-unsplash.jpg',
     b = '../src//assets/michael-jin-ET6_fDwZj2U-unsplash.jpg',
@@ -71,3 +73,30 @@ var sorte = Math.floor(Math.random() * images.length);
 console.log(sorte)
 console.log(images[sorte])
 //res.innerHTML = "<img src=" + images[sorte] + " alt='imagem'>'"
+
+/*
+const options = {
+    method: 'GET',
+    mode: 'cors',
+    cache: 'default'
+}
+
+fetch(api, options)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log('Success: ', JSON.stringify(data))
+
+        var arrayData = data
+
+        arrayData.forEach(function (element) {
+            console.log(element)
+
+
+        })
+
+    })
+    .catch(error => console.error('Error:', error))
+
+    */  
