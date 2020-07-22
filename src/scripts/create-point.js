3f4d51abd535295f83d12f5d824507d489460ae8
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-07-21 21:17:38 -0300

    refatorando

--- src/scripts/create-point.js
+++ src/scripts/create-point.js
@@ -1,18 +1,10 @@
 var entityName = document.querySelector('#entityName')
 var address = document.querySelector('#address')
 var addressNumber = document.querySelector('#address2')
-//var state = document.querySelector('select[name=uf]')
 var state = document.querySelector('#uf')
-//var city = document.querySelector('select[name=city]')
 var city = document.querySelector('#city')
 var buttonForm = document.querySelector('form')
-
-
 let modal = document.querySelector('.modal')
-
-
-//talvez tenha que pegar o ID mesmo, e na página de search, chamar a api da localização novamente
-//para tranformar o id no nome  do estado e cidade
 
 function cityEnabled(){
     city.removeAttribute('disabled')
@@ -20,81 +12,12 @@ function cityEnabled(){
 
 state.addEventListener('change', cityEnabled)
 
-
-/*
-let ufSelect = document.querySelector('select[name=uf]')
-fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome')
-    .then(function (res) {
-        return res.json()
-    })
-    .then(function (states) {
-
-        for (const state of states) {
-            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
-            
-        }
-
-
-    })
-
-
-function populateUfs() {
-    let ufSelect = document.querySelector('select[name=uf]')
-    fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome')
-        .then(function (res) {
-            return res.json()
-        })
-        .then(function (states) {
-
-            for (const state of states) {
-                ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
-            }
-
-        })
-}
-
-populateUfs()
-
-
-function getCities() {
-    let citySelect = document.querySelector('select[name=city]')
-    let stateInput = document.querySelector('input[name=state]')
-
-    const ufValue = event.target.value
-
-    const indexOfSelectedState = event.target.selectedIndex
-    stateInput.value = event.target.options[indexOfSelectedState].text
-
-    let url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
-
-    citySelect.innerHTML = ''
-    citySelect.disabled = true
-
-    fetch(url)
-        .then(function (res) {
-            return res.json()
-        })
-        .then(function (cities) {
-
-            for (city of cities) {
-                citySelect.innerHTML += `<option value='${city.nome}'>${city.nome}</option>'`
-            }
-            citySelect.disabled = false
-        })
-}
-
-
-*/
-
-
 function showModalConfirm(){
     modal.classList.remove('hide')
     setTimeout(function(){
         modal.classList.add('hide')
     }, 2750)
 } 
-
-
 
 const ItemsToCollect = document.querySelectorAll('.items-grid li')
 
@@ -107,22 +30,14 @@ let selectedItems = []
 
 function handleSelectedItem() {
     const itemLi = event.target
-    //adcionar ou remover uma clase
     itemLi.classList.toggle("selected")
 
     const itemId = itemLi.dataset.id
-
-
-    //verificar se existem itens selecionados. se sim
-    //pegar os itens selecionados
 
     let alreadySelected = selectedItems.findIndex(function (item) {
         let itemsFound = item == itemId
         return itemsFound
     })
-
-
-    //se já estiver seleiconado, tirar da seleção
 
     if (alreadySelected >= 0) {
         let filteredItems = selectedItems.filter(function (item) {
@@ -131,13 +46,9 @@ function handleSelectedItem() {
         })
         selectedItems = filteredItems
     } else {
-        //se não estiver selecionado, adicionar a seleção
-        //adicionar a seleção
         selectedItems.push(itemId)
-
     }
 
-    //atializar o campo escondido com os itens selecionados
     collectedItems.value = selectedItems
 }
 
@@ -152,7 +63,6 @@ buttonForm.addEventListener('submit', function(event){
     console.log(selectedItems)
 
     let body = {
-        //"id": 3,
         "ponto": entityName.value,
         "address": address.value,
         "address2": addressNumber.value,
@@ -175,15 +85,4 @@ buttonForm.addEventListener('submit', function(event){
     .catch(error => console.error('Error:', error))
 
     showModalConfirm()
-
 })
-
-//modal
-
-/*
-modal.style.display = 'none'
-
-let modalConfirnm = setTimeout(function(){
-    modal.style.display = 'none'
-}, 2000)
-*/
--- src/scripts/search-results.js
+++ src/scripts/search-results.js
@@ -1,7 +1,6 @@
 const api = 'http://localhost:8000/posts'
 var card = document.querySelector('#card')
 
-
 var images = [
     image1 = './src//assets/pawel-czerwinski-RkIsyD_AVvc-unsplash.jpg',
     image2 = './src//assets/michael-jin-ET6_fDwZj2U-unsplash.jpg',
@@ -10,7 +9,6 @@ var images = [
 
 console.log(images.length)
 
-images.forEach()
 var random = Math.floor(Math.random() * images.length);
 
 const options = {
@@ -38,65 +36,6 @@ fetch(api, options)
                 + '<h3>' + element.items + '</h3>' + "<br>"
                 + '<p>' + element.state + ', ' + element.city + ', ' + ' <br> '
                 + element.address + 'nº ' + element.address2 
-            
         })
-
-        /*
-        var arrayData = data
-    
-        arrayData.forEach(function(element){
-            console.log(element)
-    
-            res.innerHTML += "<li>" + "<span class='spanLiPoint'>" + element.ponto  +"</span>"+ "<br>" 
-            + '<span class="spanItemsDonate">' + element.items + '</span>' +"<br>"       
-    
-           + '<span class="spanAddress">' + 'endereço ' + '</span>' +  '<br>' + 
-           element.state + ' - ' +  element.city +"<br>"  
-           + element.address + ', ' + 'nº '  + element.address2 + "<br>"  
-            "</li>"     
-        })
-        */
-
     })
     .catch(error => console.error('Error:', error))
-
-
-/*
-var images = [
-    a = '../src//assets/pawel-czerwinski-RkIsyD_AVvc-unsplash.jpg',
-    b = '../src//assets/michael-jin-ET6_fDwZj2U-unsplash.jpg',
-    c = '../src//assets/javier-huedo-io3poUllunQ-unsplash.jpg'
-]
-
-console.log(images.length)
-var sorte = Math.floor(Math.random() * images.length);
-console.log(sorte)
-console.log(images[sorte])
-//res.innerHTML = "<img src=" + images[sorte] + " alt='imagem'>'"
-
-/*
-const options = {
-    method: 'GET',
-    mode: 'cors',
-    cache: 'default'
-}
-
-fetch(api, options)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log('Success: ', JSON.stringify(data))
-
-        var arrayData = data
-
-        arrayData.forEach(function (element) {
-            console.log(element)
-
-
-        })
-
-    })
-    .catch(error => console.error('Error:', error))
-
-    */  
